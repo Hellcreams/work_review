dfe122c939612be8995dfc11575cc865c82d60fe
Hellcreams <cconny2002@gmail.com>
2026-02-18 19:34:26 +0900

    2025.02.18

--- cpp/0_cpp_intro/Hello.cpp
+++ cpp/0_cpp_intro/Hello.cpp
@@ -2,19 +2,19 @@
 #include <cstdlib>
 
 using std::cout;
-using std::endl; // < will print \n, and flush.
-//using std::flush; // << will not print \n, just flush.
+using std::endl; // << \n을 포함하여 flush 해주는 기능.
+//using std::flush; // << flush (출력 화면으로 띄움) 만.
 
 int main(int argc, char *argv[]) {
 	std::cout << "Hello, world!" << std::endl;
 	cout << "Bye, world!" << endl;
-	return EXIT_SUCCESS;  // that is 0
+	return EXIT_SUCCESS;  // 0
 }
 
 /*
  * 1. C++'s printing
- *   - C uses "printf".
- *   - C++ starts with std:cout, ends with std.endl.
+ *   - C 는 "printf".
+ *   - C++ 는 std::cout 으로 시작하여, std::endl 으로 끝남.
  * 2. << meaning
  *   - << is Insert Operator, not Bitwise Operator (in this case).
  * 3. EXIT_SUCCESS
--- cpp/0_cpp_intro/HelloMe.cpp
+++ cpp/0_cpp_intro/HelloMe.cpp
@@ -9,7 +9,7 @@ using std::flush;
 int main() {
 	cout << "Please enter your first name: " << flush;
 
-	// string : no need to allocate by oneself (unlike C)
+	// string : C와 다르게, 크기를 지정해 줄 필요가 없다!
 	string name;
 	cin >> name;
 
@@ -21,8 +21,7 @@ int main() {
 	const string::size_type cols = greeting.size() + pad * 2 + 2;
 	/*
 	 * Defensive Programming
-	 * "I can't believe myself. so I will use "const" for variable
-	 * that dont need to change."
+	 * 내 스스로를 믿을 수 없으니.. 혹은 바뀔 이유가 없는 변수까지 const 선언.
 	 * Pros : Human Error Prevention / Self-Documentatiton / Compiler Optimization
 	 */
 	
@@ -55,3 +54,9 @@ int main() {
 }
 
 // ./a.out >> a.txt wont work normally...
+/*
+Q. C++와, Python/Java의 문자열 동작 방식 차이?
+Situation : a = "1" 이였는데, a = "12"로 바뀜.
+C++ -> a가 차지하고 있는 메모리를 직접 수정. 만약 "12" 처럼 늘어나 공간이 부족하면, 다른 여유로운 메모리 공간으로 이사. (mutable)
+Java/Python -> 새 공간에 "12"를 만들고, a가 그 공간을 가리키게 만듬. / 기존 "1"은, GC에 의해 삭제. (사용자는 편리하지만.. 그만큼 오버헤드가 많고 속도가 느림.)
+*/
